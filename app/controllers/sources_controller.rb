8e4ffb8b47b0bfadeab87ed954d130027ce4621d
damandak <damandak@uc.cl>
2021-05-30 22:24:12 -0400

    Referencias en andinistas, cerros y rutas

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -13,19 +13,38 @@ class SourcesController < ApplicationController
   # GET /sources/new
   def new
     @source = Source.new
+    if params[:mountain]
+      @referenceable = referenceable_object("Mountain", params[:mountain])
+    elsif params[:route]
+      @referenceable = referenceable_object("Route", params[:route])
+    elsif params[:ascent]
+      @referenceable = referenceable_object("Ascent", params[:ascent])
+    elsif params[:andinist]
+      @referenceable = referenceable_object("Andinist", params[:andinist])
+    end
   end
 
   # GET /sources/1/edit
   def edit
+    if params[:mountain]
+      @referenceable = referenceable_object("Mountain", params[:mountain])
+    elsif params[:route]
+      @referenceable = referenceable_object("Route", params[:route])
+    elsif params[:ascent]
+      @referenceable = referenceable_object("Ascent", params[:ascent])
+    elsif params[:andinist]
+      @referenceable = referenceable_object("Andinist", params[:andinist])
+    end
   end
 
   # POST /sources or /sources.json
   def create
     @source = Source.new(source_params)
-
+    @referenceable = referenceable_object(@source.referenceable_type, @source.referenceable_id)
+    
     respond_to do |format|
       if @source.save
-        format.html { redirect_to @source, notice: "Source was successfully created." }
+        format.html { redirect_to @referenceable, notice: "Se creó la referencia." }
         format.json { render :show, status: :created, location: @source }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -36,9 +55,11 @@ class SourcesController < ApplicationController
 
   # PATCH/PUT /sources/1 or /sources/1.json
   def update
+    @referenceable = referenceable_object(@source.referenceable_type, @source.referenceable_id)
+    
     respond_to do |format|
       if @source.update(source_params)
-        format.html { redirect_to @source, notice: "Source was successfully updated." }
+        format.html { redirect_to @referenceable, notice: "Se actualizó la referencia." }
         format.json { render :show, status: :ok, location: @source }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,9 +70,10 @@ class SourcesController < ApplicationController
 
   # DELETE /sources/1 or /sources/1.json
   def destroy
+    @referenceable = referenceable_object(@source.referenceable_type, @source.referenceable_id)
     @source.destroy
     respond_to do |format|
-      format.html { redirect_to sources_url, notice: "Source was successfully destroyed." }
+      format.html { redirect_to @referenceable, notice: "Se eliminó la referencia." }
       format.json { head :no_content }
     end
   end
@@ -62,8 +84,22 @@ class SourcesController < ApplicationController
       @source = Source.find(params[:id])
     end
 
+    def referenceable_object(obj_type, obj_id)
+      if obj_type == "Mountain"
+        return Mountain.find(obj_id)
+      elsif obj_type == "Route"
+        return Route.find(obj_id)
+      elsif obj_type == "Andinist"
+        return Andinist.find(obj_id)
+      elsif obj_type == "Ascent"
+        return Ascent.find(obj_id)
+      else
+        return nil
+      end
+    end
+
     # Only allow a list of trusted parameters through.
     def source_params
-      params.require(:source).permit(:name, :link)
+      params.require(:source).permit(:name, :link, :referenceable_id, :referenceable_type)
     end
 end
