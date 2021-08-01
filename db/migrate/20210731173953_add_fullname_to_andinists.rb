4123f9a5975662522916936034aad46e525849e0
damandak <damandak@uc.cl>
2021-07-31 21:20:42 -0400

    fix migration activaba metodos que requerian otras migraciones

--- db/migrate/20210731173953_add_fullname_to_andinists.rb
+++ db/migrate/20210731173953_add_fullname_to_andinists.rb
@@ -1,9 +1,9 @@
 class AddFullnameToAndinists < ActiveRecord::Migration[6.0]
   def change
     add_column :andinists, :fullname, :string
+
     Andinist.all.each do |andinist|
-      andinist.fullname = [andinist.name,andinist.surname].join(" ")
-      andinist.save!
+      ascent.update_column(:fullname, [andinist.name,andinist.surname].join(" "))
     end
   end
 end
