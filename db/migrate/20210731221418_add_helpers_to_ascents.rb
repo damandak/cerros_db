f4fb40ff0a4a9228e5cd38619d059c1b3127eb55
damandak <damandak@uc.cl>
2021-07-31 21:38:14 -0400

    rerefix migrations

--- db/migrate/20210731221418_add_helpers_to_ascents.rb
+++ db/migrate/20210731221418_add_helpers_to_ascents.rb
@@ -28,5 +28,23 @@ class AddHelpersToAscents < ActiveRecord::Migration[6.0]
       ascent.update_column(:fulldate, date_string)
       ascent.update_column(:fulldate_for_form, date_string_form)
     end
+
+    Ascent.all.each do |ascent|
+      route = ascent.route
+      mountain = route.mountain
+      if not route.unregistered_sport_ascent and ascent.route.ascents.sort_by(&:fulldate).first == ascent
+        ascent.update_column(:first_ascent, true)
+        if not mountain.unregistered_sport_ascent and ascent.route.mountain.ascents.sort_by(&:fulldate).first == ascent then
+          ascent.update_column(:first_absolute, true)
+        end
+      end
+    end
+
+
+    Andinist.all.each do |andinist|
+      andinist.update_column(:remarkable_ascents, andinist.ascents.count)
+      andinist.update_column(:first_absolutes, andinist.ascents.where(:first_absolute => true).count)
+      andinist.update_column(:first_ascents, andinist.ascents.where(:first_ascent => true).count)
+    end
   end
 end
