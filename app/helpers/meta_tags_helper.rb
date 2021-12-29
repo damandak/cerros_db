e1eec9baeb2c6a57209239bc259eca06eb2971bd
damandak <damandak@uc.cl>
2021-12-29 19:25:55 -0300

    meta main url

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -3,6 +3,10 @@ module MetaTagsHelper
     content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
   end
 
+  def meta_url
+    content_for?(:meta_url) ? content_for(:meta_url) : DEFAULT_META["meta_url"]
+  end
+
   def meta_description
     content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
   end
